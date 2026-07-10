3453002140f369956a3907169aae0930d8fbd0e8
JP-0506 <panchaljp73@gmail.com>
2026-07-10 23:31:46 +0530

    Register user route changes

--- node_backend/src/routes/index.js
+++ node_backend/src/routes/index.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import AuthRoutes from "./AuthRoutes.js";
 import UserRoutes from "./UserRoutes.js";
+import WorkspaceRoutes from "./WorkspaceRoutes.js";
+import ProjectRoutes from "./ProjectRoutes.js";
+import DatasetRoutes from "./DatasetRoutes.js";
 
 const router = Router();
 
@@ -14,6 +17,16 @@ router.get("/", (req, res) => {
 
 // Authentication Routes
 router.use("/api/auth", AuthRoutes);
+
+// User Routes
 router.use("/api/users", UserRoutes);
+
+// Workspace Routes
+router.use("/api/workspaces", WorkspaceRoutes);
+
+// Project Routes
+router.use("/api/projects", ProjectRoutes);
+
+router.use("/api/datasets", DatasetRoutes);
 
 export default router;
