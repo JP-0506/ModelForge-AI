03dee9fb16a6cdcb0679bdb729c0314efa182585
JP-0506 <panchaljp73@gmail.com>
2026-07-11 17:12:42 +0530

    feat(django): configure project URL routing

--- django_backend/config/urls.py
+++ django_backend/config/urls.py
@@ -1,5 +1,24 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "ModelForge AI Django Backend Running"
+    })
 
 urlpatterns = [
+     path("", home),
+    path("admin/", admin.site.urls),
 
+    path("api/ml/", include("ml_engine.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
