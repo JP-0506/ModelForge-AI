47e8c7a9ff4813c3d4cf2af7d5c43ffd6ff5353e
JP-0506 <panchaljp73@gmail.com>
2026-07-21 19:24:10 +0530

    feat(training): add trained model

---
+++ node_backend/src/models/TrainedModel.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+const trainedModelSchema = new mongoose.Schema(
+  {
+    experiment_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Experiment",
+      required: true,
+    },
+
+    model_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    model_version: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+
+    model_path: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    model_size: {
+      type: Number,
+      default: 0,
+    },
+
+    is_best_model: {
+      type: Boolean,
+      default: false,
+    },
+
+    deployment_status: {
+      type: String,
+      enum: [
+        "not_deployed",
+        "deployed",
+        "archived",
+      ],
+      default: "not_deployed",
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "active",
+        "inactive",
+      ],
+      default: "active",
+    },
+
+    is_deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+    versionKey: false,
+  }
+);
+
+const TrainedModel = mongoose.model(
+  "TrainedModel",
+  trainedModelSchema,
+);
+
+export default TrainedModel;
