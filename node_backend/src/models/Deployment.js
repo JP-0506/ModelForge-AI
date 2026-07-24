e3d874b191816b3a46e49d19d07e3752c6435cb4
JP-0506 <panchaljp73@gmail.com>
2026-07-24 23:49:37 +0530

    refactor(deployment): remove deleted timestamp from deployment model

--- node_backend/src/models/Deployment.js
+++ node_backend/src/models/Deployment.js
@@ -40,10 +40,6 @@ const deploymentSchema = new mongoose.Schema(
             default: false,
         },
 
-        deleted_at: {
-            type: Date,
-            default: null,
-        },
     },
     {
         timestamps: {
